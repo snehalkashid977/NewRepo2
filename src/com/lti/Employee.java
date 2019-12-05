b4ef4cec3c963f49e0ba3a2129f3c81c6c5d9e1a
Snehal Kashid <Snehal.Kashid@lntinfotech.com>
2019-12-05 23:09:40 +0530

    hiiiiii

---
+++ src/com/lti/Employee.java
@@ -0,0 +1,43 @@
+package com.lti;
+
+public class Employee {
+
+	private int eid;
+	private String fname;
+	private String lname;
+
+	private String doj;
+	private String dept;
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public String getDoj() {
+		return doj;
+	}
+	public void setDoj(String doj) {
+		this.doj = doj;
+	}
+	
+	
+}
